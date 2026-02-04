5cd523ce10b25873fef69a30733dcf22c1013b2f
VoktexYT <ubguertin@gmail.com>
2026-02-04 15:36:01 -0500

    Update readme.md

--- android-studio/flake.nix
+++ android-studio/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
