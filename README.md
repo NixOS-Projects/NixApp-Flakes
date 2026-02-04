51961da564f9837d57210852e430ad642856d42b
VoktexYT <ubguertin@gmail.com>
2026-02-04 15:26:59 -0500

    Add Android-Studio app

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # NixApp-Flakes
+
+### Install the repository
+```
+git clone <URL>
+```
+
+### Copy/Paste the flake files in your directory
+```
+cp <app>/flake.* <output>
+```
+
+### Enter into flake environment
+```
+nix develop
+```
+
+#### !! IMPORTANT !! Add these lines in your configuration.nix
+> nix.settings.experimental-features = [ "nix-command" "flakes" ];
---
+++ android-studio/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Android Studio + Kotlin";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "android-dev-shell";
+
+        buildInputs = with pkgs; [
+          android-studio
+          jdk17          
+        ];
+
+        shellHook = ''
+          echo "Android Studio Environment Loaded"
+          
+          export JAVA_HOME=${pkgs.jdk17}         
+        '';
+      };
+    };
+}
